93517ed412e8d2140b0d6d5e08675086987132b0
crow2673 <andrew@echo-X570-Taichi>
2026-03-12 21:01:22 -0500

    Remove system files accidentally committed, update gitignore

--- var/lib/vastai_kaalia/update_launcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~vastai_kaalia
-wget -q https://s3.amazonaws.com/public.vast.ai/kaalia/daemons$(cat ~/.channel 2>/dev/null)/update -O install_update.sh
-
-chmod +x install_update.sh
-
-./install_update.sh "$@"
